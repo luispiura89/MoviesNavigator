56437142895bf5dcfdf9d0369d2ebc60a0fb71d4
luispiura89 <luispiura@unitec.edu>
2021-11-16 14:38:42 -0600

    Initial Commit

---
+++ MoviesNavigator/MoviesNavigator.h
@@ -0,0 +1,18 @@
+//
+//  MoviesNavigator.h
+//  MoviesNavigator
+//
+//  Created by Luis Francisco Piura Mejia on 16/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MoviesNavigator.
+FOUNDATION_EXPORT double MoviesNavigatorVersionNumber;
+
+//! Project version string for MoviesNavigator.
+FOUNDATION_EXPORT const unsigned char MoviesNavigatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MoviesNavigator/PublicHeader.h>
+
+
