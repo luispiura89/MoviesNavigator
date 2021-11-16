1511c9ada5c1759edf5ca05b005e514f67ce84f3
luispiura89 <luispiura@unitec.edu>
2021-11-16 15:52:34 -0600

    Create `TVShow` model

---
+++ TVShows/Domain/TVShow.swift
@@ -0,0 +1,13 @@
+//
+//  TVShow.swift
+//  TVShows
+//
+//  Created by Luis Francisco Piura Mejia on 16/11/21.
+//
+
+public struct TVShow {
+    public let id: Int
+    public let name: String
+    public let overview: String
+    public let voteAverage: Double
+}
