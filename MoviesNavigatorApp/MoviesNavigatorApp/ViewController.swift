b6153643ee9404d5ba37d9be195af56d7a3495b2
luispiura89 <luispiura@unitec.edu>
2021-12-27 16:23:53 -0600

    Create workspace to manage the application

---
+++ MoviesNavigatorApp/MoviesNavigatorApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesNavigatorApp
+//
+//  Created by Luis Francisco Piura Mejia on 27/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
