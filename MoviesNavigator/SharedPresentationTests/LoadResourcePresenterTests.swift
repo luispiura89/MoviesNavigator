d0fb00e3a6c26e737cb1ea0b6f722cb70fb38a52
luispiura89 <luispiura@unitec.edu>
2022-01-10 17:32:28 -0600

    `LoadResourcePresenter` sends error message to error view

--- MoviesNavigator/SharedPresentationTests/LoadResourcePresenterTests.swift
+++ MoviesNavigator/SharedPresentationTests/LoadResourcePresenterTests.swift
@@ -16,37 +16,64 @@ public protocol LoadingView {
     func update(_ viewModel: LoadingViewModel)
 }
 
+public struct ErrorViewModel {
+    public let message: String
+}
+
+public protocol ErrorView {
+    func update(_ viewModel: ErrorViewModel)
+}
+
 public class LoadResourcePresenter {
     
     private let loadingView: LoadingView
+    private let errorView: ErrorView
     
-    public init(loadingView: LoadingView) {
+    public static var generalError = "Something went wrong"
+    
+    public init(loadingView: LoadingView, errorView: ErrorView) {
         self.loadingView = loadingView
+        self.errorView = errorView
     }
     
     public func didStartLoadingResource() {
         loadingView.update(LoadingViewModel(isLoading: true))
     }
     
+    public func didFinishLoading(with error: Error) {
+        loadingView.update(LoadingViewModel(isLoading: false))
+        errorView.update(ErrorViewModel(message: LoadResourcePresenter.generalError))
+    }
 }
 
 final class LoadResourcePresenterTests: XCTestCase {
     
     func test_startLoading_sendsStartLoadingMessageToView() {
         let viewSpy = ViewSpy()
-        let sut = LoadResourcePresenter(loadingView: viewSpy)
+        let sut = LoadResourcePresenter(loadingView: viewSpy, errorView: viewSpy)
         
         sut.didStartLoadingResource()
         
         XCTAssertEqual(viewSpy.messages, [.isLoading(true)])
     }
     
+    func test_completeLoadingWithError_sendsErrorMessageToErrorView() {
+        let viewSpy = ViewSpy()
+        let error = anyError()
+        let sut = LoadResourcePresenter(loadingView: viewSpy, errorView: viewSpy)
+        
+        sut.didFinishLoading(with: error)
+        
+        XCTAssertEqual(viewSpy.messages, [.isLoading(false), .error(LoadResourcePresenter.generalError)])
+    }
+    
     // MARK: - Helpers
     
-    private final class ViewSpy: LoadingView {
+    private final class ViewSpy: LoadingView, ErrorView {
         
         enum Message: Equatable {
             case isLoading(Bool)
+            case error(String)
         }
         
         private(set) var messages = [Message]()
@@ -54,6 +81,14 @@ final class LoadResourcePresenterTests: XCTestCase {
         func update(_ viewModel: LoadingViewModel) {
             messages.append(.isLoading(viewModel.isLoading))
         }
+        
+        func update(_ viewModel: ErrorViewModel) {
+            messages.append(.error(viewModel.message))
+        }
+    }
+    
+    private func anyError() -> NSError {
+        NSError(domain: "Error", code: 0, userInfo: nil)
     }
     
 }
