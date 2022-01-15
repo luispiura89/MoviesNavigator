1680c971b9a8d56162102ba0b225c325d24e5961
luispiura89 <luispiura@unitec.edu>
2022-01-14 19:06:29 -0600

    Add documentation for snapshot tests

--- MoviesNavigator/TVShowsiOSTests/Helpers/UIViewController+Snapshots.swift
+++ MoviesNavigator/TVShowsiOSTests/Helpers/UIViewController+Snapshots.swift
@@ -14,6 +14,12 @@ extension UIViewController {
 }
 
 struct SnapshotConfiguration {
+    /// Values such as
+    /// size, safeAreaInsets, layoutMargins can be taken from the window created n the SceneDelegate as follows
+    /// `windowScene.keyWindow?.safeAreaInsets` -> safeAreaInsets
+    /// `windowScene.keyWindow?.layoutMargins` -> layoutMargins
+    /// `windowScene.keyWindow?.bounds` -> size
+    /// The `traitCollection` can be taken from `UIScreen.main.traitCollection`
     let size: CGSize
     let safeAreaInsets: UIEdgeInsets
     let layoutMargins: UIEdgeInsets
@@ -28,6 +34,25 @@ struct SnapshotConfiguration {
             size: CGSize(width: 390, height: 844),
             safeAreaInsets: UIEdgeInsets(top: 47, left: 0, bottom: 34, right: 0),
             layoutMargins: UIEdgeInsets(top: 55, left: 8, bottom: 42, right: 8),
+            traitCollection: UITraitCollection(traitsFrom: [
+                .init(forceTouchCapability: .unavailable),
+                .init(layoutDirection: .leftToRight),
+                .init(preferredContentSizeCategory: contentSize),
+                .init(userInterfaceIdiom: .phone),
+                .init(horizontalSizeClass: .compact),
+                .init(verticalSizeClass: .regular),
+                .init(displayScale: 3),
+                .init(accessibilityContrast: .normal),
+                .init(displayGamut: .P3),
+                .init(userInterfaceStyle: style)
+            ]))
+    }
+    
+    static func iPhone8Plus(style: UIUserInterfaceStyle, contentSize: UIContentSizeCategory = .medium) -> SnapshotConfiguration {
+        return SnapshotConfiguration(
+            size: CGSize(width: 414, height: 736),
+            safeAreaInsets: UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0),
+            layoutMargins: UIEdgeInsets(top: 28, left: 8, bottom: 8, right: 8),
             traitCollection: UITraitCollection(traitsFrom: [
                 .init(forceTouchCapability: .unavailable),
                 .init(layoutDirection: .leftToRight),
