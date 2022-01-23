bf293eb88b5a18bdf60a11c5f2f8472201ea7dd9
luispiura89 <luispiura@unitec.edu>
2022-01-22 18:22:18 -0600

    Modify the status handling in the `TVShowCellController`

--- MoviesNavigator/TVShowsiOS/TV Shows/TV Shows Cell Controllers/TVShowCellController.swift
+++ MoviesNavigator/TVShowsiOS/TV Shows/TV Shows Cell Controllers/TVShowCellController.swift
@@ -17,7 +17,15 @@ public final class TVShowCellController: NSObject, UICollectionViewDataSource {
     private let viewModel: TVShowViewModel
     private var cell: TVShowHomeCell?
     private let delegate: TVShowCellControllerDelegate?
-    private var loadedImage: UIImage?
+    
+    private enum State: Equatable {
+        case loading
+        case failed
+        case completed(UIImage?)
+        case none
+    }
+    
+    private var state: State = .none
     
     public init(viewModel: TVShowViewModel, delegate: TVShowCellControllerDelegate?) {
         self.viewModel = viewModel
@@ -31,34 +39,48 @@ public final class TVShowCellController: NSObject, UICollectionViewDataSource {
     public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         if cell == nil {
             cell = (collectionView.dequeueReusableCell(withReuseIdentifier: TVShowHomeCell.dequeueIdentifier, for: indexPath) as? TVShowHomeCell)
+            cell?.retryActionHandler = { [weak self] in
+                self?.state = .none
+                self?.downloadImageIfNeeded()
+            }
         }
-        
         cell?.nameLabel.text = viewModel.name
         cell?.dateLabel.text = viewModel.firstAirDate
         cell?.voteAverageLabel.text = viewModel.voteAverage
         cell?.overviewLabel.text = viewModel.overview
-        if loadedImage == nil {
-            delegate?.requestImage()
-        } else {
-            cell?.posterImageView.image = loadedImage
-        }
+        state == .loading ? cell?.loadingView.startAnimating() : cell?.loadingView.stopAnimating()
+        cell?.retryLoadingButton.isHidden = state != .failed
+        downloadImageIfNeeded()
         return cell!
     }
     
     public func setPosterImage(_ image: UIImage?) {
-        loadedImage = image
         cell?.posterImageView.image = image
         cell?.retryLoadingButton.isHidden = true
         cell?.loadingView.stopAnimating()
+        state = .completed(image)
     }
     
     public func setLoadingErrorState() {
         cell?.posterImageView.image = nil
         cell?.retryLoadingButton.isHidden = false
         cell?.loadingView.stopAnimating()
+        state = .failed
     }
     
     public func setLoadingState() {
         cell?.loadingView.startAnimating()
+        state = .loading
+    }
+    
+    private func downloadImageIfNeeded() {
+        switch state {
+        case .completed(let loadedImage):
+            cell?.posterImageView.image = loadedImage
+        case .none:
+            delegate?.requestImage()
+        default:
+            break
+        }
     }
 }
