692d4b4787350d37b4399798b8b550e86bc9a72d
luispiura89 <luispiura@unitec.edu>
2022-02-16 10:33:50 -0600

    Refactor `StubHTTPClient` to be self contained

--- MoviesNavigatorApp/MoviesNavigatorAppTests/Acceptance Tests/HomeScreenAcceptanceTests.swift
+++ MoviesNavigatorApp/MoviesNavigatorAppTests/Acceptance Tests/HomeScreenAcceptanceTests.swift
@@ -15,7 +15,7 @@ import SharedAPI
 final class HomeScreenAcceptanceTests: XCTestCase {
     
     func test_app_shouldRenderHomeShowsForOnlineMode() {
-        let sut = launch(client: HomeScreenAcceptanceTests.StubHTTPClient(stub: makeSuccessfulResponse))
+        let sut = launch(client: .online)
         
         XCTAssertEqual(sut.renderedCells(), 2)
         
@@ -32,7 +32,7 @@ final class HomeScreenAcceptanceTests: XCTestCase {
     
     // MARK: - Helpers
     
-    private func launch(client: HTTPClient) -> HomeViewController {
+    private func launch(client: StubHTTPClient) -> HomeViewController {
         let scene = SceneDelegate(httpClient: client)
         scene.window = UIWindow()
         scene.configure()
@@ -42,78 +42,94 @@ final class HomeScreenAcceptanceTests: XCTestCase {
     
     private final class StubHTTPClient: HTTPClient {
         
-        private var stub: () -> GetResult
+        private var stub: (URL) -> GetResult
         
-        init(stub: @escaping () -> GetResult) {
+        static var online: StubHTTPClient { StubHTTPClient(stub: Self.makeSuccessfulResponse) }
+        
+        init(stub: @escaping (URL) -> GetResult) {
             self.stub = stub
         }
         
         func get(from url: URL, completion: @escaping GetCompletion) {
-            completion(stub())
+            completion(stub(url))
         }
         
-    }
-    
-    private func makeTVShow(
-        id: Int,
-        name: String,
-        overview: String,
-        voteAverage: Double,
-        firstAirDate: String,
-        posterPath: URL?
-    ) -> [String: Any] {
+        private static func makeSuccessfulResponse(for url: URL) -> HTTPClient.GetResult {
+            let urlComponents = URLComponents(string: url.absoluteString)
+            let host = urlComponents?.host ?? ""
+            let path = urlComponents?.path ?? ""
+            switch host {
+            case "api.themoviedb.org" where path.contains("tv"):
+                return .success((
+                    makeResultsJSON(page: 1, results: [
+                        makeTVShow(
+                            id: 0,
+                            name: "First show",
+                            overview: "First overview",
+                            voteAverage: 5.0,
+                            firstAirDate: "2022-01-15",
+                            posterPath: URL(string: "https://image.tmdb.org/t/p/w1280/image-1.jpg")),
+                        makeTVShow(
+                            id: 1,
+                            name: "Second show",
+                            overview: "Second overview",
+                            voteAverage: 6.0,
+                            firstAirDate: "2022-01-16",
+                            posterPath: URL(string: "https://image.tmdb.org/t/p/w1280/image-2.jpg"))
+                    ]),
+                    HTTPURLResponse(
+                        url: URL(string: "https://any-url.com")!,
+                        statusCode: 200,
+                        httpVersion: nil,
+                        headerFields: nil
+                    )!
+                ))
+            default:
+                return .success((
+                    Data(),
+                    HTTPURLResponse(
+                        url: URL(string: "https://any-url.com")!,
+                        statusCode: 200,
+                        httpVersion: nil,
+                        headerFields: nil
+                    )!
+                ))
+            }
+        }
         
-        let model = TVShow(
-            id: id,
-            name: name,
-            overview: overview,
-            voteAverage: voteAverage,
-            firstAirDate: firstAirDate,
-            posterPath: posterPath)
+        private static func makeTVShow(
+            id: Int,
+            name: String,
+            overview: String,
+            voteAverage: Double,
+            firstAirDate: String,
+            posterPath: URL?
+        ) -> [String: Any] {
+            
+            let model = TVShow(
+                id: id,
+                name: name,
+                overview: overview,
+                voteAverage: voteAverage,
+                firstAirDate: firstAirDate,
+                posterPath: posterPath)
+            
+            var json: [String: Any] = [:]
+            json["id"] = model.id
+            json["name"] = model.name
+            json["overview"] = model.overview
+            json["vote_average"] = model.voteAverage
+            json["first_air_date"] = model.firstAirDate
+            json["poster_path"] = posterPath?.absoluteString
+            
+            return json.compactMapValues { $0 }
+        }
         
-        var json: [String: Any] = [:]
-        json["id"] = model.id
-        json["name"] = model.name
-        json["overview"] = model.overview
-        json["vote_average"] = model.voteAverage
-        json["first_air_date"] = model.firstAirDate
-        json["poster_path"] = posterPath?.absoluteString
-        
-        return json.compactMapValues { $0 }
+        private static func makeResultsJSON(page: Int, results: [[String: Any]]) -> Data {
+            try! JSONSerialization.data(withJSONObject: [
+                "page": page,
+                "results": results
+            ])
+        }
     }
-    
-    private func makeResultsJSON(page: Int, results: [[String: Any]]) -> Data {
-        try! JSONSerialization.data(withJSONObject: [
-            "page": page,
-            "results": results
-        ])
-    }
-    
-    private func makeSuccessfulResponse() -> HTTPClient.GetResult {
-        .success((
-            makeResultsJSON(page: 1, results: [
-                makeTVShow(
-                    id: 0,
-                    name: "First show",
-                    overview: "First overview",
-                    voteAverage: 5.0,
-                    firstAirDate: "2022-01-15",
-                    posterPath: URL(string: "https://poster1.com")),
-                makeTVShow(
-                    id: 1,
-                    name: "Second show",
-                    overview: "Second overview",
-                    voteAverage: 6.0,
-                    firstAirDate: "2022-01-16",
-                    posterPath: URL(string: "https://poster2.com"))
-            ]),
-            HTTPURLResponse(
-                url: URL(string: "https://any-url.com")!,
-                statusCode: 200,
-                httpVersion: nil,
-                headerFields: nil
-            )!
-        ))
-    }
-    
 }
