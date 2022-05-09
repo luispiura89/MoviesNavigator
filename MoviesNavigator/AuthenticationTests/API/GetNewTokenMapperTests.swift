04bba5561f6eedeb46959bdae14f457b6f56f7e5
Luis Piura <luisf.piura@hotmail.com>
2022-05-09 17:34:32 -0600

    NewTokenRequestMapper.map maps session token on 200 response and valid data

--- MoviesNavigator/AuthenticationTests/API/GetNewTokenMapperTests.swift
+++ MoviesNavigator/AuthenticationTests/API/GetNewTokenMapperTests.swift
@@ -13,11 +13,29 @@ enum AuthenticationError: Error {
 }
 
 public final class NewTokenRequestMapper {
-    
-    public static func map(_ data: Data, for response: HTTPURLResponse) throws {
-        throw AuthenticationError.invalidData
+    private struct CodableSessionToken: Codable {
+        let expires_at: String
+        let request_token: String
+        
+        var sessionToken: SessionToken {
+            SessionToken(requestToken: request_token, expiresAt: expires_at)
+        }
     }
     
+    public static func map(_ data: Data, for response: HTTPURLResponse) throws -> SessionToken {
+        guard response.statusCode == 200,
+              let token = try? JSONDecoder().decode(CodableSessionToken.self, from: data).sessionToken else {
+            throw AuthenticationError.invalidData
+        }
+        
+        return token
+    }
+    
+}
+
+public struct SessionToken {
+    public let requestToken: String
+    public let expiresAt: String
 }
 
 final class GetNewTokenMapperTests: XCTestCase {
@@ -36,9 +54,27 @@ final class GetNewTokenMapperTests: XCTestCase {
         XCTAssertThrowsError(try NewTokenRequestMapper.map(anyData(), for: HTTPURLResponse(code: 200)), "") { error in
             XCTAssertEqual(error as? AuthenticationError, AuthenticationError.invalidData)
         }
-        XCTAssertThrowsError(try NewTokenRequestMapper.map(non200HTTPResponseData(), for: HTTPURLResponse(code: 200)), "") { error in
+        XCTAssertThrowsError(
+            try NewTokenRequestMapper.map(
+                non200HTTPResponseData(),
+                for: HTTPURLResponse(code: 200)
+            ),
+            ""
+        ) { error in
             XCTAssertEqual(error as? AuthenticationError, AuthenticationError.invalidData)
         }
+    }
+    
+    func test_map_shouldDeliverNewTokenOn200HTTPResponseAndValidData() throws {
+        let token = "any-token"
+        let expirationDate = "2022-05-10 00:07:44 UTC"
+        let sessionToken =
+        try NewTokenRequestMapper.map(
+            successfulHTTPResponseData(withToken: token, expirationDate: expirationDate),
+            for: HTTPURLResponse(code: 200)
+        )
+        XCTAssertEqual(sessionToken.requestToken, token)
+        XCTAssertEqual(sessionToken.expiresAt, expirationDate)
     }
     
     // MARK: - Helpers
@@ -52,6 +88,15 @@ final class GetNewTokenMapperTests: XCTestCase {
             "status_code": 7,
             "status_message": "Any error message",
             "success": false
+        ]
+        return try JSONSerialization.data(withJSONObject: json)
+    }
+    
+    private func successfulHTTPResponseData(withToken token: String, expirationDate: String) throws -> Data {
+        let json: [String: Any] = [
+            "expires_at": expirationDate,
+            "request_token": token,
+            "success": true
         ]
         return try JSONSerialization.data(withJSONObject: json)
     }
