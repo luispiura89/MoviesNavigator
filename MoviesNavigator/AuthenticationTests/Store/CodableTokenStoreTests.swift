5670f646fff700b57f877b15ff0c38cf1a91ed4b
Luis Piura <luisf.piura@hotmail.com>
2022-05-12 15:16:32 -0600

    `CodableTokenStore.fetchToken` delivers store token after succesful insertion

--- MoviesNavigator/AuthenticationTests/Store/CodableTokenStoreTests.swift
+++ MoviesNavigator/AuthenticationTests/Store/CodableTokenStoreTests.swift
@@ -14,45 +14,117 @@ public enum TokenStoreError: Error {
 
 public struct StoredToken {
     public let token: String
-    public let date: Date
+    public let expirationDate: Date
 }
 
 public final class CodableTokenStore {
+    
+    private let storeURL: URL
+    
+    init(storeURL: URL) {
+        self.storeURL = storeURL
+    }
+    
+    private struct CodableStoredToken: Codable {
+        let token: String
+        let expirationDate: Date
+    }
     
     public typealias FetchTokenResult = Result<String, Error>
     public typealias FetchTokenCompletion = (FetchTokenResult) -> Void
     
     public typealias StoreTokenResult = Result<Void, Error>
-    public typealias StoreTokenCompletion = (FetchTokenResult) -> Void
+    public typealias StoreTokenCompletion = (StoreTokenResult) -> Void
     
     public func fetch(completion: @escaping FetchTokenCompletion) {
-        completion(.failure(TokenStoreError.emptyStore))
+        guard let storedTokenData = try? Data(contentsOf: storeURL) else {
+            return completion(.failure(TokenStoreError.emptyStore))
+        }
+        completion(
+            Result {
+                let storedToken = try JSONDecoder().decode(CodableStoredToken.self, from: storedTokenData)
+                return storedToken.token
+            }.mapError { _ in
+                TokenStoreError.writeOperationFailed
+            }
+        )
     }
     
     public func store(_ token: StoredToken, completion: @escaping StoreTokenCompletion) {
-        completion(.failure(TokenStoreError.writeOperationFailed))
+        completion(
+            Result{
+                let codableStoredToken = CodableStoredToken(
+                    token: token.token,
+                    expirationDate: token.expirationDate
+                )
+                let encoder = JSONEncoder()
+                let data = try encoder.encode(codableStoredToken)
+                try data.write(to: storeURL)
+            }.mapError { _ in
+                TokenStoreError.writeOperationFailed
+            }
+        )
     }
     
 }
 
 final class CodableTokenStoreTests: XCTestCase {
     
+    override func setUp() {
+        super.setUp()
+        clearSideEffects()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        clearSideEffects()
+    }
+    
     func test_store_deliversEmptyTokenErrorWhenThereIsNoStoredToken() {
-        let sut = CodableTokenStore()
+        let sut = makeSUT(withURL: storeURL())
         
         expectStoredToken(in: sut, toBeEqualsTo: nil)
     }
     
     func test_store_deliversEmptyTokenErrorWhenTokenStoreOperationFailed() {
-        let instanceToRead = CodableTokenStore()
-        let instanceToWrite = CodableTokenStore()
+        let instanceToRead = makeSUT(withURL: storeURL())
+        let instanceToWrite = makeSUT(withURL: cachesDirectory())
         
-        store(token: StoredToken(token: "any-token", date: Date()), in: instanceToWrite)
+        store(token: StoredToken(token: "any-token", expirationDate: Date()), in: instanceToWrite)
         
         expectStoredToken(in: instanceToRead, toBeEqualsTo: nil)
     }
     
+    func test_store_deliversStoredTokenWhenThereIsAStoredToken() {
+        let instanceToRead = makeSUT(withURL: storeURL())
+        let instanceToWrite = makeSUT(withURL: storeURL())
+        let token = StoredToken(token: "any-token", expirationDate: Date())
+        
+        store(token: token, in: instanceToWrite)
+        
+        expectStoredToken(in: instanceToRead, toBeEqualsTo: token.token)
+    }
+    
     // MARK: - Helpers
+    
+    private func clearSideEffects() {
+        try? FileManager.default.removeItem(at: storeURL())
+    }
+    
+    private func makeSUT(withURL url: URL) -> CodableTokenStore {
+        return CodableTokenStore(storeURL: url)
+    }
+    
+    private func cachesDirectory() -> URL {
+        FileManager
+            .default
+            .urls(for: .documentDirectory, in: .userDomainMask)
+            .first!
+    }
+    
+    private func storeURL() -> URL {
+        cachesDirectory().appendingPathComponent("\(type(of: self)).store")
+    }
     
     private func expectStoredToken(
         in store: CodableTokenStore,
