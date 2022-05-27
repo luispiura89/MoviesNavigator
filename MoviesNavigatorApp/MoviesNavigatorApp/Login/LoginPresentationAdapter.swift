6001d108f8a110a49e8e33c6ed97e6821db0909f
Luis Piura <luisf.piura@hotmail.com>
2022-05-27 14:53:40 -0600

    Move class to its own file

---
+++ MoviesNavigatorApp/MoviesNavigatorApp/Login/LoginPresentationAdapter.swift
@@ -0,0 +1,54 @@
+//
+//  LoginPresentationAdapter.swift
+//  MoviesNavigatorApp
+//
+//  Created by Luis Francisco Piura Mejia on 27/5/22.
+//
+
+import SharedPresentation
+import Authentication
+import AuthenticationiOS
+import Combine
+
+typealias LoginPresenter = LoadResourcePresenter<SessionToken, LoginViewAdapter>
+public typealias LoginPublisherHandler = (String, String) -> AnyPublisher<SessionToken, Error>
+
+final class LoginPresentationAdapter: LoginLoadingViewControllerDelegate {
+    
+    var presenter: LoginPresenter?
+    private let loginPublisher: LoginPublisherHandler
+    private var user = ""
+    private var password = ""
+    private var cancellable: AnyCancellable?
+    private var inProgress = false
+    
+    init(loginPublisher: @escaping LoginPublisherHandler) {
+        self.loginPublisher = loginPublisher
+    }
+    
+    func update(user: String) {
+        self.user = user
+    }
+    
+    func update(password: String) {
+        self.password = password
+    }
+    
+    func sendLoginRequest() {
+        guard !inProgress else { return }
+        inProgress.toggle()
+        presenter?.didStartLoadingResource()
+        cancellable = loginPublisher(user, password)
+            .dispatchOnMainQueue()
+            .sink { [weak self] completion in
+                if case let .failure(error) = completion {
+                    self?.presenter?.didFinishLoading(with: error)
+                }
+                self?.inProgress.toggle()
+            } receiveValue: { [weak self] token in
+                self?.presenter?.didFinishLoading(with: token)
+                self?.inProgress.toggle()
+            }
+    }
+    
+}
