df99cc430c50d7056fb3fe24e51d48fe7d8f339f
Luis Piura <luisf.piura@hotmail.com>
2022-05-31 17:49:48 -0600

    LoginEndpoint generates body params for login

--- MoviesNavigator/AuthenticationTests/API/LoginEndpointTests.swift
+++ MoviesNavigator/AuthenticationTests/API/LoginEndpointTests.swift
@@ -18,6 +18,9 @@ public enum LoginEndpoint {
         static let new = "/new"
         static let validateWithLogin = "/validate_with_login"
         static let apiKey = "api_key"
+        static let userName = "username"
+        static let password = "password"
+        static let requestToken = "request_token"
     }
     
     public func getURL(from baseURL: URL, apiKey: String) -> URL {
@@ -36,6 +39,20 @@ public enum LoginEndpoint {
         
         return urlComponents?.url ?? baseURL
     }
+    
+    public func getParameters(_ params: String...) -> [String: Any]? {
+        switch self {
+        case .validateTokenWithLogin:
+            guard params.count == 3 else { return nil }
+            var bodyParams = [String: Any]()
+            bodyParams[Constants.userName] = params[0]
+            bodyParams[Constants.password] = params[1]
+            bodyParams[Constants.requestToken] = params[2]
+            return bodyParams
+        default:
+            return nil
+        }
+    }
 }
 
 final class LoginEndpointTests: XCTestCase {
@@ -53,6 +70,20 @@ final class LoginEndpointTests: XCTestCase {
         XCTAssertEqual(components?.path, "/3/authentication/token/validate_with_login")
         XCTAssertEqual(components?.queryItems?.first?.name, "api_key")
         XCTAssertEqual(components?.queryItems?.first?.value, "123456789")
+    }
+    
+    func test_getURL_generatesValidBodyParamsForValidateToken() {
+        let endpoint: LoginEndpoint = .validateTokenWithLogin
+        
+        let params = endpoint.getParameters("username", "password", "token")
+        XCTAssertEqual(params?["username"] as? String, "username")
+        XCTAssertEqual(params?["request_token"] as? String, "token")
+    }
+    
+    func test_getURL_generatesInvalidBodyParamsForValidateTokenWithNotEnoughData() {
+        let endpoint: LoginEndpoint = .validateTokenWithLogin
+        
+        XCTAssertNil(endpoint.getParameters("username"))
     }
     
     func test_getURL_generatesValidURLForNewToken() {
